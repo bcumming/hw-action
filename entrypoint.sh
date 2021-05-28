431c7b249efb02b997cd5c3040af765e712aa300
bcumming <bcumming@cscs.ch>
2021-05-28 11:13:24 +0200

    bump

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,33 @@
 #!/bin/sh -l
 
-echo '---------------- ls ---------------- '
+echo '---------------- ls ----------------'
 ls
-echo '---------------- pwd --------------- '
+echo '---------------- pwd ---------------'
 pwd
-echo '---------------- printenv ---------- '
+echo '---------------- printenv ----------'
 printenv
-echo '---------------- GITHUB_WORKSPACE --'
-echo $GITHUB_WORKSPACE
-echo '---------------- GITHUB_ENV --------'
-echo $GITHUB_ENV
+echo '---------------- /github/home -------'
+ls /github/home
+echo '---------------- /github/workflow ---'
+ls /github/workflow
+echo '---------------- /github/workspace --'
+ls /github/workspace
 
 echo "hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
+
+#GITHUB_WORKFLOW
+#GITHUB_HEAD_REF
+#GITHUB_BASE_REF
+#GITHUB_EVENT_NAME
+#GITHUB_SERVER_URL
+#GITHUB_API_URL
+#GITHUB_GRAPHQL_URL
+#GITHUB_WORKSPACE
+#GITHUB_ACTION
+#GITHUB_EVENT_PATH
+#GITHUB_ACTION_REPOSITORY
+#GITHUB_ACTION_REF
+#GITHUB_PATH
+#GITHUB_ENV
