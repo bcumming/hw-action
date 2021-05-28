0712f166e300b4e31373f65c55d8bb7928c7fb29
bcumming <bcumming@cscs.ch>
2021-05-28 10:57:23 +0200

    print environment information

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/sh -l
 
+echo '---------------- ls ---------------- '
+ls
+echo '---------------- pwd --------------- '
+pwd
+echo '---------------- printenv ---------- '
+printenv
+echo '---------------- GITHUB_WORKSPACE --'
+echo $GITHUB_WORKSPACE
+echo '---------------- GITHUB_ENV --------'
+echo $GITHUB_ENV
+
 echo "hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
